e0921605cca6afb91f99e74d6d2de2e54c4e37c3
000-123-456 <moisesgabrielhernamdes@gmail.com>
2023-07-24 14:26:21 -0600

    probando

--- proyectoweb/contacto/views.py
+++ proyectoweb/contacto/views.py
@@ -5,12 +5,13 @@ def contacto(request):
 
    formulario_contacto=FormularioContacto()
 
-   if request.method=="post":
-      formulario_contacto=FormularioContacto(data=request.POST)
+   if request.method=="POST":
+      formulario_contacto=FormularioContacto(data=request.post)
       if formulario_contacto.is_valid():
          nombre=request.post.get("nombre")
          email=request.post.get("email")
          contenido=request.post.get("contenido")
 
-         return redirect("/contacto/?valido")
+         return redirect("/contacto/?aseptado")
+      
    return render(request, "contacto/contacto.html", {'miformulario':formulario_contacto})

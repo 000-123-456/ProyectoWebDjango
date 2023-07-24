f8c7e489840e19a2e6664173043198b7dd91b840
000-123-456 <moisesgabrielhernamdes@gmail.com>
2023-07-24 14:17:56 -0600

    formulario contacto

--- proyectoweb/contacto/views.py
+++ proyectoweb/contacto/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import FormularioContacto
 # Create your views here.
 def contacto(request):
 
    formulario_contacto=FormularioContacto()
 
+   if request.method=="post":
+      formulario_contacto=FormularioContacto(data=request.POST)
+      if formulario_contacto.is_valid():
+         nombre=request.post.get("nombre")
+         email=request.post.get("email")
+         contenido=request.post.get("contenido")
+
+         return redirect("/contacto/?valido")
    return render(request, "contacto/contacto.html", {'miformulario':formulario_contacto})
